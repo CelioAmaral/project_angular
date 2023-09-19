b1c385adff85eaeb2b76b2011fb3113768255396
CelioAmaral <celioamaralibf@gmail.com>
2023-09-19 20:50:23 -0300

    first commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,50 @@
+export const dataFake = [
+  {
+    'id': '1',
+    'photoCover': 'https://s.glbimg.com/og/rg/f/original/2011/04/06/100610114510spirit_wallpa.jpg',
+    'title': 'LEIA A SINOPSE DO FILME SPIRIT STALLION OF THE CIMARRON',
+    'description': 'No final do século XVII em pleno Oeste norte-americano vive Spirit, um cavalo que resiste a ser domado pelo homem. Ele se apaixona por uma égua local, chamada Chuva, e desenvolve uma grande amizade com um jovem índio Lakota chamado Pequeno Rio. Juntos eles acompanham a colonização do local onde vivem, percebendo as mudanças que a chegada da civilização fazem em seu dia-a-dia.',
+    'descriptionTwo': 'Com música original escrita e cantada por Bryan Adams, Spirit: Stallion of the Cimarron tocará seu coração e o deixará na ponta da cadeira.'
+  },
+  {
+    'id': '2',
+    'photoCover': 'https://4.bp.blogspot.com/-ziyUaabo9dA/VapjvxHZdSI/AAAAAAAAAL8/U63EAzbV_SU/s1600/02.jpg',
+    'title': '"CRÍTICA DO FILME SEGUNDO IANN JELIEL NO SITE PLANOCRITICO',
+    'description': 'De modo genérico, a animação de Kelly Asbury & Lorna Cook é sobre a amizade de um cavalo e índio em busca mútua pela liberdade natural que escolheram para seu modo de vida, mas há muito mais pano de fundo nessa união, quando pensamos a maneira que o americano valoriza parcialmente sua história de liberdade, a qual, é formada por inúmeras resistências não devidamente valorizadas ou descaracterizadas de sua origem. Se tratando de uma animação predominantemente infantil do início dos anos 2000, é fantástico como Spirit lida com discursões bem complexas utilizando o domo do cavalo como metáfora aos debates de choques culturais que historicamente aconteceram em território americano, trazendo um recorte que não exatamente resolve alguma coisa daquele universo, mas adquiri o status de resistência universal a qual o brasão foi inicialmente pensado a representar.',
+    'descriptionTwo': 'Desde cenas em que os cavalos puxando objetos remetendo-se ao trabalho escravo, até as guerrilhas entre coloniais invadindo terras indígenas que fornece a perseguição principal a reger a aventura, existe sempre um forte subtexto histórico circundando a proposta de faroeste animado, direcionada também para um âmbito naturalista, onde as paisagens são quase um personagem para promover um discurso poderoso de preservação ecológica, no qual, obviamente, conversa com a cultura indígena dos nativos americanos, como também, é um parâmetro para preservar a estética realista almejada.  A animação aposta em não trazer animais falantes como sua concorrente Disney, além de trabalhar em seu design o máximo de veracidade possível nos movimentos dos bichos, contudo, sem deixar com que isso atrapalhe a liberdade lúdica do desenho. Pelo contrário, se a jornada do filme é em busca da liberdade, essa liberdade lúdica vai crescendo conforme a necessidade do aumento de escala da narrativa, que possui uma ação muito bem distribuída e cada vez mais empolgante conforme o decorrer da duração.',
+    'descriptionThree': 'Não chega a ser tão grandiosa ou arriscada quanto O Príncipe do Egito nos parâmetros da Dreamworks, pois existe a narração em off do cavalo protagonista (voz de Matt Damon) para termos uma noção do que ele está sentido em momentos pontuais, além das canções posicionadas em momentos estratégicos apresentarem explicações bem diretas do que é narrado em imagem. No entanto, há uma dosagem perfeita desse didatismo a torná-lo uma ferramenta de simplificação eficiente ao emocional da narrativa puramente imagética, além de um equilíbrio da expressividade do facial do desenho com sua movimentação realista. A montagem torna Spirit: O Corcel Indomável um épico, mesmo com a pouca duração, porque valoriza sua proposta artística no máximo potencial da via dramática, com uma leveza sóbria na condução que também valoriza a comicidade, deixando a seriedade da história devidamente acessível a todos os públicos. É um casamento ideal entre maturidade com pureza. O desenvolvimento da amizade acaba sendo o ponto de interseção para que esses extremos se adequem com coesão. Conquista pela simplicidade, mas ganha seu poder numa representatividade maior, a qual a empresa foi cada vez mais assumindo em sua identidade a partir de Shrek.',
+    'descriptionFour': 'Nesse aspecto, a animação acaba sendo um divisor de águas da Dreamworks, trazendo a ápice da criatividade artística que visava aproximar o estúdio dos demais grandes a qualquer custo, com o ápice do que ela veio a se tornar padronizada com suas histórias conduzidas por carismáticas de amizades entre “diferentes” que historicamente nunca tiveram tanta voz e podem hoje ser reconhecidos por ela, depois de muito esforço – como o próprio coronel, antagonista até genérico e simbólico do filme, reconhece depois daquele salto “impossível”, o clímax onde aparece a liberdade animada que mencionei. Bom saber depois de ter visto a última fez bem novo na infância, na memória como uma das minhas animações favoritas, que Spirit: O Corcel Indomável sobrevive e muito a uma mera nostalgia particular, e é realmente o símbolo perfeito, inesquecível e subestimado de um subestimado estúdio de animação.'
+  },
+  {
+    'id': '3',
+    'photoCover': 'https://3.bp.blogspot.com/-lraIoEnTTRg/VaphWcJuRrI/AAAAAAAAALQ/NyXiCzN2AnE/s1600/spirit.jpg',
+    'title': 'TRÊS CURIOSIDADES SOBRE O FILME',
+    'description': 'Curiosidade 1: Spirit é o longa-metragem de estreia de Kelly Asbury como diretora.',
+    'descriptionTwo': 'Curiosidade 2: Spirit é o 2º longa-metragem de animação dublado pelo ator Matt Damon. O anterior foi Titan A.E. (2000).',
+    'descriptionThree': 'Curiosidade 3: Nas versões dubladas de Spirit as músicas do filme são cantadas por Paulo Ricardo, enquanto que na versão original do filme elas são cantadas por Bryan Adams.'
+  },
+  {
+    'id': '4',
+    'photoCover': 'https://triibo.com.br/wp-content/uploads/2022/02/Imagem-Post-Comunidades-Triibo-2560-px-%C3%97-1789-px-4-1.png',
+    'title': 'TRÊS DETALHES QUE VOCÊ NÃO SABIA DESSE FILME',
+    'description': 'Detalhe 1: Os desenhistas passaram anos estudando os movimentos dos cavalos, seu trote e como correm. Tudo para transpor nas telas movimentos realísticos. Também realísticos são os relinchos dos cavalos, uma vez que não há diálogos entre os animais. Mas há uma imponente narração do cavalo protagonista, na voz de Matt Damon. Muitas são as animações que tem certas partes emocionantes, mas raríssimas são as que já começam assim. ‘Spirit’ tem já na sua abertura cenas de arrepiar.',
+    'descriptionTwo': 'Detalhe 2: Durante o filme são várias as cenas aéreas impressionantes, vistas através de uma águia, que tanto vem a ser o amigo com quem Spirit corre quanto que também serve como uma metáfora para a liberdade. Todo visual do filme é impressionante, muito bem rascunhado, cheio de bela paisagens e extremamente bem desenhado a mão, na antiga modalidade 2D.',
+    'descriptionThree': 'Detalhe 3: É impossível falar de ‘Spirit’ sem comentar sobre sua impactante trilha sonora. Equivalendo a um musical, grande parte da narrativa é desenvolvida em cima da letra das canções. No áudio original, temos o talentoso Bryan Adams. Na versão brasileira, a voz igualmente boa de Paulo Ricardo. Foi uma decisão sábia colocarem Paulo Ricardo, ele é o melhor representante do estilo de Bryan Adams, um dos poucos cantores de nosso país que preencheria esta lacuna. As canções passam as emoções e pensamentos de Spirit, seus medos e sua luta.'
+  },
+  {
+    'id': '5',
+    'photoCover': 'https://images6.fanpop.com/image/photos/33100000/-Spirit-Stallion-of-the-Cimarron-spirit-stallion-of-the-cimarron-33182223-1000-690.jpg',
+    'title': 'HOMENAGENS FEITAS AO FILME.',
+    'description': 'Indicado ao Oscar de Melhor Animação e perdendo injustamente, o filme é uma obra-prima vinda da Dreamworks, e não da Disney como alguns acham.',
+    'descriptionTwo': 'Considero o melhor filme da produtora. ‘Spirit’ é para a DreamWorks o que ‘O Rei Leão’ é para a Disney, sua animação máxima, levando em conta a emoção e a maturidade da obra. Um filme que merece ser revisitado várias vezes.',
+    'descriptionThree': 'Spirit: O Corcel Indomável traz um ser puro, uma força que apenas quer manter o que tem de mais valioso: seu espírito de liberdade.'
+  },
+  {
+    'id': '6',
+    'photoCover': 'https://2.bp.blogspot.com/-1idzfpvlvlQ/VxZ2sBcAbWI/AAAAAAAANAI/pX2hNfKBk_kWCCRXeAPRst6ySB6YU2pYACLcB/s1600/spirit2.jpg',
+    'title': '3 LIÇÕES QUE APRENDEMOS COM O FILME',
+    'description': 'A forma como ela mostra como os animais, a natureza e os nativos americanos sofreram com a civilização a que foram forçados.',
+    'descriptionTwo': 'Além disso, um dos pontos altos do desenho é que indo na contramão do que se costuma ver, os animais não falam. Eles na verdade se comunicam através de seus sons e linguagens corporais como acontece na natureza.',
+    'descriptionThree': 'O Corcel Indomável sempre estará em meu coração é por como ele nos ensina a sempre lutar pelo que acreditamos. Assim como nos mostra que nunca devemos desistir de voltar para aqueles que amamos. Por isso, não deixem de dar uma conferida nessa animação inesquecível.'
+  }
+]
